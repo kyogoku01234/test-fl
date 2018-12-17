7eee2e92236b3bc6229007d231f5584791b91fd2
kyogoku01234 <41919648+kyogoku01234@users.noreply.github.com>
2018-12-17 21:16:12 +0700

    upcode

--- Freelancer/Form1.cs
+++ Freelancer/Form1.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Security.Principal;
@@ -21,15 +22,29 @@ namespace Freelancer
 
             InitializeComponent();
             this.loadPage();
+            this.AddListUser();
         }
 
         public void loadPage()
         {
             webBrowser1.Navigate("http://10.149.34.168/ccbs/login.htm");
+
         }
+
+        private void AddListUser()
+        {
+            richTextBox1.AppendText("\r\n dddd");
+            richTextBox1.AppendText("\r\n dddd");
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            var x = webBrowser1.Version;           
+            Object[] objArray = new Object[2];
+            objArray[0] = "1";
+            objArray[1] = "10";
+            webBrowser1.Document.InvokeScript("test", objArray);
+
+            webBrowser2.Document.InvokeScript("layds_thuebao2", objArray);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -37,17 +52,39 @@ namespace Freelancer
             webBrowser2.Navigate("http://10.149.34.168/ccbs/main?1iutlomLork=vzzh5rgvnj5inutyu5otjk~");
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        public void login(string user, string pass)
         {
-            string user = textBox1.Text;
-            string pass = textBox2.Text;
-
             var userElements = webBrowser1.Document.GetElementById("username");
             var passElements = webBrowser1.Document.GetElementById("passWord");
             var clickElements = webBrowser1.Document.GetElementById("btnLogin");
             userElements.InnerText = user;
             passElements.InnerText = pass;
             clickElements.InvokeMember("click");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+            login(user, pass);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"\script.txt");
+            var x = File.ReadAllText(@"E:\script.txt");
+
+            HtmlDocument doc = webBrowser2.Document;
+            HtmlElement head = doc.GetElementsByTagName("head")[0];
+            HtmlElement s = doc.CreateElement("script");
+            s.SetAttribute("text", x);
+            head.AppendChild(s);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
